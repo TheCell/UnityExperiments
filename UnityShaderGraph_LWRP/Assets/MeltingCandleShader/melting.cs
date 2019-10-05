ac04974078ff766bdc11b0c632ba0a7b96bee3bd
TheCell <simon_hischier@hotmail.com>
2019-10-05 19:22:41 +0200

    tried the melting candle tutorial

---
+++ UnityShaderGraph_LWRP/Assets/MeltingCandleShader/melting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class melting : MonoBehaviour
+{
+	private Material candleMaterial;
+	[SerializeField] private Transform flamePos;
+	private float amount;
+	public Transform flameStartPos;
+	public Transform flameEndPos;
+	//private Vector3 startScale;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		candleMaterial = gameObject.GetComponent<Renderer>().material;
+		//startScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		amount = 1 - (Time.timeSinceLevelLoad / 3) % 1;
+		candleMaterial.SetFloat("_Amount", amount);
+		if (flamePos != null)
+		{
+			flamePos.position = Vector3.Lerp(flameEndPos.position, flameStartPos.position, amount);
+		}
+		//transform.localScale = new Vector3(startScale.x, startScale.y * amount, startScale.z);
+	}
+}
