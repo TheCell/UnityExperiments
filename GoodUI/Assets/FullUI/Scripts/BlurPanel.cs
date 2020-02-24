d3a3d71b275756baa573569c4a5e341be71207ac
TheCell <simon_hischier@hotmail.com>
2020-02-25 00:07:20 +0100

    UIBuilding

---
+++ GoodUI/Assets/FullUI/Scripts/BlurPanel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(CanvasGroup))]
+[AddComponentMenu("UI/Blur Panel")]
+public class BlurPanel : Image
+{
+    public bool animate;
+    public float time = 0.5f;
+    public float delay = 0f;
+
+    private CanvasGroup canvas;
+
+    protected override void Reset()
+    {
+        base.Reset();
+        //color = Color.black * 0.1f;
+    }
+
+    protected override void Awake()
+    {
+        //base.Awake();
+        canvas = GetComponent<CanvasGroup>();
+    }
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        if (Application.isPlaying)
+        {
+            canvas.alpha = 0;
+            StartCoroutine("Fader");
+        }
+    }
+
+    private IEnumerator Fader()
+    {
+        float startTime = Time.realtimeSinceStartup + delay;
+        while (Time.realtimeSinceStartup < startTime)
+        {
+            yield return null;
+        }
+
+        while (Time.realtimeSinceStartup < startTime + time)
+        {
+            canvas.alpha = Mathf.Lerp(0, 1, Mathf.InverseLerp(startTime, startTime + time, Time.realtimeSinceStartup));
+            yield return null;
+        }
+
+        canvas.alpha = 1.0f;
+    }
+}
---
+++ GoodUI/Assets/FullUI/Scripts/Busybackground.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Busybackground : MonoBehaviour
+{
+    private float randomRotation;
+
+    private void Start()
+    {
+        randomRotation = Random.Range(-15f, 15f);
+    }
+    private void Update()
+    {
+        transform.rotation = transform.rotation * Quaternion.Euler(0, randomRotation * Time.deltaTime, 0);
+    }
+}
