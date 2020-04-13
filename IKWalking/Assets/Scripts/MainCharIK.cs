d144f32c6ef0a1bbb0b44a94a43c57c426230929
TheCell <simon_hischier@hotmail.com>
2020-04-13 17:46:27 +0200

    more testing, fixing forward and up bone transforms

--- IKWalking/Assets/Scripts/MainCharIK.cs
+++ IKWalking/Assets/Scripts/MainCharIK.cs
@@ -9,15 +9,41 @@ public class MainCharIK : MonoBehaviour
 	[SerializeField] private Transform lookTarget;
 	// A reference to the gecko's neck
 	[SerializeField] private Transform headBone;
-	[SerializeField] private float headRotationSpeed;
+	[SerializeField] private float headRotationSpeed = 0.1f;
+	//[SerializeField] private float headMaxTurnAngle = 40f;
+
+	private void Start()
+	{
+	}
 
 	// We will put all our animation code in LateUpdate.
 	// This allows other systems to update the environment first, 
 	// allowing the animation system to adapt to it before the frame is drawn.
 	void LateUpdate()
 	{
-		Vector3 towardObjectFromHead = lookTarget.position - headBone.position;
-		Quaternion targetRotation = Quaternion.LookRotation(towardObjectFromHead, transform.up);
+		//Quaternion currentLocalRotation = headBone.localRotation;
+		Quaternion currentRotation = headBone.rotation;
+		// localrot is not zero in my case
+
+		Vector3 targetWorldLookDir = lookTarget.position - headBone.position;
+		//Vector3 targetLocalLookDir = headBone.InverseTransformDirection(targetWorldLookDir);
+
+		//targetLocalLookDir = Vector3.RotateTowards(
+		//	Vector3.forward,
+		//	targetLocalLookDir,
+		//	Mathf.Deg2Rad * headMaxTurnAngle,
+		//	0
+		//);
+
+		//Quaternion targetLocalRotation = Quaternion.LookRotation(targetLocalLookDir, Vector3.up);
+		//headBone.localRotation = targetLocalRotation;
+		//headBone.localRotation = Quaternion.Slerp(
+		//		currentLocalRotation,
+		//		targetLocalRotation,
+		//		1 - Thecelleu.Utilities.Damp(1, headRotationSpeed, Time.deltaTime)
+		//	);
+
+		Quaternion targetRotation = Quaternion.LookRotation(targetWorldLookDir, transform.up);
 		headBone.rotation = Quaternion.Slerp(headBone.rotation, targetRotation, 1 - Thecelleu.Utilities.Damp(1, headRotationSpeed, Time.deltaTime));
 	}
 }
