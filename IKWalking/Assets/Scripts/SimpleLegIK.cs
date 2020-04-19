5d7b4de9823f10905668354ec52a6080840f772c
TheCell <simon_hischier@hotmail.com>
2020-04-19 19:11:23 +0200

    fake IK half working

---
+++ IKWalking/Assets/Scripts/SimpleLegIK.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleLegIK : MonoBehaviour
+{
+    [SerializeField] private Transform kneeTarget;
+    [SerializeField] private Transform legOrigin;
+    [SerializeField] private Transform lowerLegOrigin;
+
+    [SerializeField] private Transform footPlacement;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        UpdateLegPos();
+    }
+
+    private void UpdateLegPos()
+    {
+        legOrigin.transform.LookAt(kneeTarget, legOrigin.transform.up);
+        lowerLegOrigin.transform.LookAt(footPlacement, legOrigin.transform.up);
+
+    }
+}
---
+++ IKWalking/Assets/Scripts/SimpleMoveScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMoveScript : MonoBehaviour
+{
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+        }
+    }
+}
