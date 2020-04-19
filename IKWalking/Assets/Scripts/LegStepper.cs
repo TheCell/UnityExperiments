312723002be82ba586a198382e4909b1bbdcba4d
TheCell <simon_hischier@hotmail.com>
2020-04-19 19:26:35 +0200

    walking somehow working

--- IKWalking/Assets/Scripts/LegStepper.cs
+++ IKWalking/Assets/Scripts/LegStepper.cs
@@ -7,6 +7,8 @@ public class LegStepper : MonoBehaviour
 	[SerializeField] Transform homeTransform;
 	[SerializeField] float wantStepAtDistance;
 	[SerializeField] float moveDuration;
+	[SerializeField] float stepOvershootFraction;
+
 	private bool isMoving;
 
 	private void Update()
@@ -31,15 +33,28 @@ public class LegStepper : MonoBehaviour
 		Quaternion startRot = transform.rotation;
 		Vector3 startPoint = transform.position;
 		Quaternion endRot = homeTransform.rotation;
-		Vector3 endPoint = homeTransform.position;
+		//Vector3 endPoint = homeTransform.position;
 		float timeElapsed = 0f;
+
+		Vector3 towardHome = (homeTransform.position - transform.position);
+		float overshootDistance = wantStepAtDistance * stepOvershootFraction;
+		Vector3 overshootVector = towardHome * overshootDistance;
+		overshootVector = Vector3.ProjectOnPlane(overshootVector, Vector3.up);
+		Vector3 endPoint = homeTransform.position + overshootVector;
+		Vector3 centerPoint = (startPoint + endPoint) / 2;
+		centerPoint += homeTransform.up * Vector3.Distance(startPoint, endPoint) / 2f;
 
 		do
 		{
 			timeElapsed += Time.deltaTime;
 			float normalizedTime = timeElapsed / moveDuration;
 
-			transform.position = Vector3.Lerp(startPoint, endPoint, normalizedTime);
+			transform.position = 
+				Vector3.Lerp(
+					Vector3.Lerp(startPoint, centerPoint, normalizedTime),
+					Vector3.Lerp(centerPoint, endPoint, normalizedTime),
+					normalizedTime
+					);
 			transform.rotation = Quaternion.Slerp(startRot, endRot, normalizedTime);
 
 			yield return null;
